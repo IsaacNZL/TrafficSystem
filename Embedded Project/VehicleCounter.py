c2441969999ba4a9ad8d9220e6bdd39f61c9c9e3
IsaacNZL <isaacrobbuk@hotmail.com>
2024-10-08 16:49:59 +1300

    Add files via upload

---
+++ Embedded Project/VehicleCounter.py
@@ -0,0 +1,92 @@
+# Import the Open-CV extra functionalities
+import cv2
+import os
+
+# This is to pull the information about what each object is called
+classNames = []
+classFile = r'C:\Users\nct3743\PycharmProjects\YOLOtraining\Object_Detection_Files\coco.names'
+with open(classFile, "rt") as f:
+    classNames = f.read().rstrip("\n").split("\n")
+
+# This is to pull the information about what each object should look like
+configPath = r'C:\Users\nct3743\PycharmProjects\YOLOtraining\Object_Detection_Files\ssd_mobilenet_v3_large_coco_2020_01_14.pbtxt'
+weightsPath = r'C:\Users\nct3743\PycharmProjects\YOLOtraining\Object_Detection_Files\frozen_inference_graph.pb'
+
+# This is some setup values to get good results
+net = cv2.dnn_DetectionModel(weightsPath, configPath)
+net.setInputSize(320, 320)
+net.setInputScale(1.0 / 127.5)
+net.setInputMean((127.5, 127.5, 127.5))
+net.setInputSwapRB(True)
+
+# This is to set up what the drawn box size/colour is and the font/size/colour of the name tag and confidence label
+def getObjects(img, thres, nms, draw=True, objects=[]):
+    classIds, confs, bbox = net.detect(img, confThreshold=thres, nmsThreshold=nms)
+    if len(objects) == 0: objects = classNames
+    objectInfo = []
+    if len(classIds) != 0:
+        for classId, confidence, box in zip(classIds.flatten(), confs.flatten(), bbox):
+            className = classNames[classId - 1]
+            if className in objects:
+                objectInfo.append([box, className])
+                if draw:
+                    cv2.rectangle(img, box, color=(0, 255, 0), thickness=2)
+                    cv2.putText(img, classNames[classId - 1].upper(), (box[0] + 10, box[1] + 30),
+                                cv2.FONT_HERSHEY_COMPLEX, 1, (0, 255, 0), 2)
+                    cv2.putText(img, str(round(confidence * 100, 2)), (box[0] + 200, box[1] + 30),
+                                cv2.FONT_HERSHEY_COMPLEX, 1, (0, 255, 0), 2)
+
+    return img, objectInfo
+
+
+# Load the video file
+video_path = r'C:\Users\nct3743\PycharmProjects\Embedded Project\TrafficLight.mp4'  # Update with the correct path to your video
+cap = cv2.VideoCapture(video_path)
+
+# Check if the video opened successfully
+if not cap.isOpened():
+    print("Error: Could not open video file.")
+    exit()
+
+while True:
+    ret, frame = cap.read()
+    if not ret:
+        print("End of video file or error reading frame.")
+        break
+
+    # Reset the counters for vehicles for each frame
+    car_count = 0
+    bus_count = 0
+    motorcycle_count = 0
+    bicycle_count = 0
+
+    # Run object detection on the current frame
+    result, objectInfo = getObjects(frame, 0.6, 0.2, objects=['car', 'motorcycle', 'bus', 'bicycle'])
+
+    # Count the number of detected vehicles in the current frame
+    for obj in objectInfo:
+        if obj[1] == 'car':
+            car_count += 1
+        elif obj[1] == 'bus':
+            bus_count += 1
+        elif obj[1] == 'motorcycle':
+            motorcycle_count += 1
+        elif obj[1] == 'bicycle':
+            bicycle_count += 1
+
+    # Print the counts for each vehicle type for the current frame
+    print(f"Cars: {car_count}, Buses: {bus_count}, Motorcycles: {motorcycle_count}, Bicycles: {bicycle_count}")
+
+    # Resize the frame to make it smaller (e.g., 50% of the original size)
+    frame_resized = cv2.resize(result, (int(result.shape[1] * 0.45), int(result.shape[0] * 0.45)))
+
+    # Show the resized results
+    cv2.imshow("Output", frame_resized)
+
+    # Press 'q' to exit the loop
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# Release the video capture and close all windows
+cap.release()
+cv2.destroyAllWindows()
