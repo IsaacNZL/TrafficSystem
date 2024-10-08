16a9d35fcd4f0a206992c0f84557e7396c760a73
IsaacNZL <isaacrobbuk@hotmail.com>
2024-10-08 17:38:56 +1300

    Add files via upload

--- Embedded Project/VehicleCounter.py
+++ Embedded Project/VehicleCounter.py
@@ -4,13 +4,13 @@ import os
 
 # This is to pull the information about what each object is called
 classNames = []
-classFile = r'C:\Users\nct3743\PycharmProjects\YOLOtraining\Object_Detection_Files\coco.names'
+classFile = r'/home/pi64/Desktop/Object_Detection_Files/coco.names'
 with open(classFile, "rt") as f:
     classNames = f.read().rstrip("\n").split("\n")
 
 # This is to pull the information about what each object should look like
-configPath = r'C:\Users\nct3743\PycharmProjects\YOLOtraining\Object_Detection_Files\ssd_mobilenet_v3_large_coco_2020_01_14.pbtxt'
-weightsPath = r'C:\Users\nct3743\PycharmProjects\YOLOtraining\Object_Detection_Files\frozen_inference_graph.pb'
+configPath = r'/home/pi64/Desktop/Object_Detection_Files/ssd_mobilenet_v3_large_coco_2020_01_14.pbtxt'
+weightsPath = r'/home/pi64/Desktop/Object_Detection_Files/frozen_inference_graph.pb'
 
 # This is some setup values to get good results
 net = cv2.dnn_DetectionModel(weightsPath, configPath)
@@ -40,7 +40,7 @@ def getObjects(img, thres, nms, draw=True, objects=[]):
 
 
 # Load the video file
-video_path = r'C:\Users\nct3743\PycharmProjects\Embedded Project\TrafficLight.mp4'  # Update with the correct path to your video
+video_path = r'/home/pi64/Documents/TrafficDataBase/CarVideos.mp4'  # path to video
 cap = cv2.VideoCapture(video_path)
 
 # Check if the video opened successfully
@@ -59,9 +59,10 @@ while True:
     bus_count = 0
     motorcycle_count = 0
     bicycle_count = 0
+    total_vehicles = 0
 
     # Run object detection on the current frame
-    result, objectInfo = getObjects(frame, 0.6, 0.2, objects=['car', 'motorcycle', 'bus', 'bicycle'])
+    result, objectInfo = getObjects(frame, 0.6, 0.2, objects=['car', 'motorcycle', 'bus'])
 
     # Count the number of detected vehicles in the current frame
     for obj in objectInfo:
@@ -71,11 +72,11 @@ while True:
             bus_count += 1
         elif obj[1] == 'motorcycle':
             motorcycle_count += 1
-        elif obj[1] == 'bicycle':
-            bicycle_count += 1
+
+        total_vehicles = car_count + bus_count + motorcycle_count
 
     # Print the counts for each vehicle type for the current frame
-    print(f"Cars: {car_count}, Buses: {bus_count}, Motorcycles: {motorcycle_count}, Bicycles: {bicycle_count}")
+    print(f"VehicleCount: {total_vehicles} | Cars: {car_count} | Buses: {bus_count} | Motorcycles: {motorcycle_count}")
 
     # Resize the frame to make it smaller (e.g., 50% of the original size)
     frame_resized = cv2.resize(result, (int(result.shape[1] * 0.45), int(result.shape[0] * 0.45)))
